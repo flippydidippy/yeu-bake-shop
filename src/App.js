08fcc889cacfe28276c14b947288bd7f8c15b0c6
flippydip <83292856+flippydip@users.noreply.github.com>
2022-02-05 23:58:29 +0100

    setup and structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
+import { Index, Contact, Page404 } from "./components";
 
-function App() {
-    return <div></div>;
-}
+import ScrollToTop from "./components/hooks/ScrollToTop"
+
+const App = () => {
+    return (
+        <>
+            <NavBar />
+            <ScrollToTop/>
+            <Routes>
+                
+                <Route path="/" exact element={<Index />} />
+                <Route path="/Contact" exact element={<Contact />} />
+                
+                <Route path="*" element={<Page404 />} />
+            </Routes>
+            <Footer />
+        </>
+    );
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// Components
+export { default as NavBar } from './NavBar/NavBar'
+export { default as Footer } from './Footer/Footer'
+
+// Pages
+export { default as Index } from './pages/Index/Index';
+export { default as Contact } from './pages/Contact/Contact';
+export { default as Page404 } from './pages/Page404/Page404';
